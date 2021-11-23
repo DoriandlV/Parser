555623c0d91f9acc0ff0e6a83d0718aefb0e44b0
MSI\Umberto <aizen.gin90@gmail.com>
2021-11-23 23:21:29 +0100

    Runner Class Added

--- src/main/java/com/Runner.java
+++ src/main/java/com/Runner.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 
 import java.util.List;
